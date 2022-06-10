e19793791350b5c275671331d10dd9fb1b6a5468
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-06-10 11:08:31 -0500

    first if else statement - trial

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,45 @@
+/*password length = 8 - 128 characters
+criteria: 
+uppercase Y/N
+lowercase Y/N
+numeric Y/N
+special characters Y/N
+*/
+var z = ("8", "9", "10");
 // Assignment code here
 
+    window.alert("Welome to your password generator! Click the Generate Password button to select password criteria.");
+//Person clicks generate button
+    var generate = function() {
+    var z = window.prompt("Please enter a number to be your password length.");
+    //console.log("good so far");
+  
+  if (z === "8" || z === "9" || z === "10") {
+    console.log("working");
+  }else {
+    console.log("incorrect");
+  }
+  
+  
+  
 
+  
+  
+
+  
+/*
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
+    passwordText.value = password;
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    generateBtn.addEventListener("click", writePassword);}
+*/
+  }
+  generate();
