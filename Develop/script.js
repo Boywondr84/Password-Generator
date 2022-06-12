4a5a2a970da1ed7ff80f800453b3f5c66c6d224b
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-06-12 12:06:42 -0500

    for loop code written, tested, working!

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,26 @@
 // Assignment code here
 window.alert("Click the Generate Password button to begin creating your password");
   //var generatePassword = function () {
-  //VARIABLE LIST
+//VARIABLE LIST
   const abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   const xyz = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  const special = [" ", "!", `"`, "#", "$", "%", "&", "'", "(", ")", "*", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "];
+  const special = ["!", `"`, "#", "$", "%", "&", "'", "(", ")", "*", `,`, "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"];
   var total = (">=8" && "<=128");
 
-  //INSTRUCTIONS FOR GENERATOR
+//INSTRUCTIONS FOR GENERATOR
 
   var generatePassword = function () {
-  var total = window.prompt("How many characters would you like your password to have? Enter a number between 8 - 128.");
+  var total = parseInt(window.prompt("How many characters would you like your password to have? Enter a number between 8 - 128."));
   //console.log("click works", total);
   if (total >= 8 && total <= 128) {
       console.log(total);
+      console.log(typeof(total));
   }else {
     window.alert("Incorrect choice. Try again.");
-    generatePassword();
-    //Restart
+    return;
   }
-
-    //start over function
   
-  //total = !<8 && !>128
 //Window Prompts
   let upperCase = window.confirm("Do you want to include upper case letters?");  
       if (upperCase) {
@@ -52,10 +49,10 @@ window.alert("Click the Generate Password button to begin creating your password
       }else {
         console.log("not selected");
     }
-    
+
   let allChar = []
 
-  //Password selections added to allChar [].
+//Password selections added to allChar [].
   if (upperCase) {
     allChar.push(...abc);
   }
@@ -66,20 +63,28 @@ window.alert("Click the Generate Password button to begin creating your password
 
   if (numChar) {
     allChar.push(...numerals);
-    //for (i = 0; i = total; i++) {
-      //total += total.push(math.floor(math.random*allChar.push.length);
-    //}
   }
-
 
   if (specialChar) {
     allChar.push(...special);
   }
+//Return Output criteria selected
   console.log(allChar)
 
-  //For loop
-  //let output = ""
-  //for(i=0, i<total, i++) {
+  //For loop - take all characters selected in allChar[], randomly select using selections stored in allChar
+  
+  function createPassword() {
+    var completePass = []
+    for(i=0; i<total; i++) {
+      var charIndex = Math.floor(Math.random() * allChar.length);
+      completePass.push(allChar[charIndex])
+    }
+    console.log(completePass);
+    return completePass.join("")
+  }
+
+    return createPassword();
+  
 
   //output.push(allChar[Math.rand() * allChar.length]
 
@@ -87,7 +92,7 @@ window.alert("Click the Generate Password button to begin creating your password
   //return output
 
 }
-//if / else statement
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -96,25 +101,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+if (password) {
   passwordText.value = password;
-
+}
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-/*password length = 8 - 128 characters
-criteria: 
-uppercase Y/N
-lowercase Y/N
-numeric Y/N
-special characters Y/N
-*/
-/*
-var total = (//password length 8 - 128 characters);
-const ABC = (//"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-const xyz = (//"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-const numeric = (//"0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-const special = (//" ", "!", """, "#", "$", "%", "&", "'", "(", ")", "*", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "`", "{", "}", "|", "~");
-*/
